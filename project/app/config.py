47437e34f47cb65378a0cc75175cac88d76bd7a6
Eric Vo <vo.eric@gmail.com>
2026-02-03 16:30:20 -0800

    feat: add postgres

--- project/app/config.py
+++ project/app/config.py
@@ -1,5 +1,6 @@
 import logging
 from functools import lru_cache
+from pydantic import AnyUrl
 from pydantic_settings import BaseSettings
 
 log = logging.getLogger("uvicorn")
@@ -8,6 +9,7 @@ log = logging.getLogger("uvicorn")
 class Settings(BaseSettings):
     environment: str = "dev"
     testing: bool = bool(0)
+    database_url: AnyUrl = None
 
 
 @lru_cache()
