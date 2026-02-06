be60b2ff361b51fa246eb02edde7a0f72a7e0f0b
Eric Vo <vo.eric@gmail.com>
2026-02-05 16:19:00 -0800

    feat: add new routes and related tests

--- project/tests/conftest.py
+++ project/tests/conftest.py
@@ -2,8 +2,9 @@ import os
 
 import pytest
 from starlette.testclient import TestClient
+from tortoise.contrib.fastapi import register_tortoise
 
-from app import main
+from app.main import create_application
 from app.config import get_settings, Settings
 
 
@@ -13,6 +14,23 @@ def get_settings_override():
 
 @pytest.fixture(scope="module")
 def test_app():
-    main.app.dependency_overrides[get_settings] = get_settings_override
-    with TestClient(main.app) as test_client:
+    app = create_application()
+    app.dependency_overrides[get_settings] = get_settings_override
+    with TestClient(app) as test_client:
+        yield test_client
+
+
+@pytest.fixture(scope="module")
+def test_app_with_db():
+    app = create_application()
+    app.dependency_overrides[get_settings] = get_settings_override
+    register_tortoise(
+        app,
+        db_url=os.environ.get("DATABASE_TEST_URL"),
+        modules={"models": ["app.models.tortoise"]},
+        generate_schemas=True,
+        add_exception_handlers=True,
+    )
+
+    with TestClient(app) as test_client:
         yield test_client
